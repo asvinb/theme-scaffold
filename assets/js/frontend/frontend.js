22729a707bcfd0ea17f55e092380d74acbb34e50
Asvin Balloo <asvin.balloo@10up.com>
2018-03-26 18:59:18 +0400

    Added Vue and D3 to test JS.

--- assets/js/frontend/frontend.js
+++ assets/js/frontend/frontend.js
@@ -1,1 +1,9 @@
 // import foo from './components/bar';
+import Vue from 'vue';
+import Vuex from 'vuex';
+import * as d3 from 'd3';
+
+Vue.use( Vuex );
+
+const square = d3.selectAll( 'rect' );
+square.style( 'fill', 'orange' );
